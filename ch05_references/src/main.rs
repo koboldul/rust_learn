f066d6ced96063d62b2392d6f6f706957af3587b
bcostea <bogdan.costea@microsoft.com>
2023-03-23 14:58:27 -0400

    cow poc

--- ch05_references/src/main.rs
+++ ch05_references/src/main.rs
@@ -1,3 +1,40 @@
+use std::borrow::{Cow, self};
+
+/// https://doc.rust-lang.org/std/borrow/enum.Cow.html#impl-Borrow%3CB%3E-for-Cow%3C%27a%2C%20B%3E
+
 fn main() {
-    println!("Hello, world!");
+    test_cow();
+}
+
+struct Items<'a, X: 'a> where [X]: ToOwned<Owned = Vec<X>> {
+    values: Cow<'a, [X]>
+}
+
+impl<'a, X: Clone + 'a> Items<'a, X> where [X]: ToOwned<Owned = Vec<X>> {
+    fn new(items: Cow<'a, [X]>) -> Self {
+        Items {
+            values: items 
+        }
+    }
+}
+
+fn test_cow() {
+    let readonly = [1,2];
+    let x = &readonly[..];
+    let mut borrowed = Items::new(x.into());
+
+    match borrowed {
+        Items { values: Cow::Borrowed(b)} => println!("Borrowd {b:?}"),
+        _ => panic!("nothing borrowed")
+    }
+
+    //let mut clone_on_write = borrowed;
+    borrowed.values.to_mut().push(3);
+    println!("clone_on_write = {:?}", borrowed.values);
+
+    // The data was mutated. Let's check it out.
+    match borrowed {
+        Items { values: Cow::Owned(_) } => println!("clone_on_write contains owned data"),
+        _ => panic!("expect owned data"),
+    }
 }
